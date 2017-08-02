bdc4bd3bc04cc0cd651bab43ca29e17d00e62103
Sanjana Desai <sanjanadesai@Sanjanas-MacBook-Air.local>
2017-08-02 08:44:00 -0700

    added validations for both store and employee models

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,47 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store1.employees.create(
+  first_name: "Khurram", 
+  last_name: "Virani", 
+  hourly_rate: 60 )
+
+@store1.employees.create(
+  first_name: "Joe", 
+  last_name: "Johnson", 
+  hourly_rate: 20 )
+
+@store1.employees.create(
+  first_name: "Paul", 
+  last_name: "Smith", 
+  hourly_rate: 30 )
+
+@store1.employees.create(
+  first_name: "Charles", 
+  last_name: "Dickins", 
+  hourly_rate: 38 )
+
+@store1.employees.create(
+  first_name: "Sally", 
+  last_name: "Max", 
+  hourly_rate: 55 )
+
+@store2.employees.create(
+  first_name: "Bob", 
+  last_name: "White", 
+  hourly_rate: 49 )
+
+ @store2.employees.create(
+  first_name: "David", 
+  last_name: "McDonald", 
+  hourly_rate: 50 )
+ 
+ @store2.employees.create(
+  first_name: "Jen", 
+  last_name: "James", 
+  hourly_rate: 72 )
+
+ @store2.employees.create(
+  first_name: "Mel", 
+  last_name: "Remington", 
+  hourly_rate: 37 )
--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,5 @@
 class Store < ActiveRecord::Base
-
+  has_many :employees
+  validate :name length: { min: 3 }
+  validate :annual_revenue numericality: true, greater_than_or_equal_to: 0
 end
