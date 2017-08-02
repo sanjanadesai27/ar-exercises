6a82a65d51a088785c9de4d153a54b69f75a0157
Sanjana Desai <sanjanadesai@Sanjanas-MacBook-Air.local>
2017-08-01 20:01:57 -0700

    "added stores, queried all stores, queried women's stores"

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,3 +7,35 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+Store.create(
+  name: "Surrey",
+  annual_revenue: 224000,
+  mens_apparel: false,
+  womens_apparel: true
+)
+
+Store.create(
+  name: "Whistler",
+  annual_revenue: 1900000,
+  mens_apparel: true,
+  womens_apparel: false
+)
+
+Store.create(
+  name: "Yaletown",
+  annual_revenue: 430000,
+  mens_apparel: true,
+  womens_apparel: true
+)
+
+all_stores = Store.all 
+
+all_stores.each do |store|
+  puts "#{store.name}, #{store.annual_revenue}"
+end
+
+small_womens_stores = Store.where(womens_apparel: true).where("annual_revenue < 1000000")
+
+small_womens_stores.each do |store|
+  puts "#{store.name}"
+end
